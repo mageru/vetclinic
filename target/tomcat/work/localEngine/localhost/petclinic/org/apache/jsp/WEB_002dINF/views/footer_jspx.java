8c93a145a85d5575f674f4856a72a08cc1c45a97
Justin Miller <jmiller9@gmail.com>
2013-03-14 00:15:02 -0400

    Fixed welcome message and app title

--- target/tomcat/work/localEngine/localhost/petclinic/org/apache/jsp/WEB_002dINF/views/footer_jspx.java
+++ target/tomcat/work/localEngine/localhost/petclinic/org/apache/jsp/WEB_002dINF/views/footer_jspx.java
@@ -17,12 +17,6 @@ static {
 
   private static java.util.List _jspx_dependants;
 
-  static {
-    _jspx_dependants = new java.util.ArrayList(2);
-    _jspx_dependants.add("/WEB-INF/tags/util/language.tagx");
-    _jspx_dependants.add("/WEB-INF/tags/util/theme.tagx");
-  }
-
   private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fspring_005furl_0026_005fvar_005fvalue_005fnobody;
   private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fspring_005fmessage_0026_005fcode_005fnobody;
   private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fc_005fif_0026_005ftest;
@@ -85,22 +79,6 @@ static {
       out.write("</a>");
       out.write("</span>");
       if (_jspx_meth_c_005fif_005f0(_jspx_page_context))
-        return;
-      out.write("<span id=\"language\">");
-      if (_jspx_meth_c_005fout_005f1(_jspx_page_context))
-        return;
-      if (_jspx_meth_spring_005fmessage_005f2(_jspx_page_context))
-        return;
-      if (_jspx_meth_c_005fout_005f2(_jspx_page_context))
-        return;
-      if (_jspx_meth_util_005flanguage_005f0(_jspx_page_context))
-        return;
-      if (_jspx_meth_util_005flanguage_005f1(_jspx_page_context))
-        return;
-      if (_jspx_meth_util_005flanguage_005f2(_jspx_page_context))
-        return;
-      out.write("</span>");
-      if (_jspx_meth_util_005ftheme_005f0(_jspx_page_context))
         return;
       if (_jspx_meth_spring_005furl_005f2(_jspx_page_context))
         return;
@@ -284,135 +262,6 @@ static {
       _jspx_th_spring_005fmessage_005f1.doFinally();
       _005fjspx_005ftagPool_005fspring_005fmessage_0026_005fcode_005fnobody.reuse(_jspx_th_spring_005fmessage_005f1);
     }
-    return false;
-  }
-
-  private boolean _jspx_meth_c_005fout_005f1(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  c:out
-    org.apache.taglibs.standard.tag.rt.core.OutTag _jspx_th_c_005fout_005f1 = (org.apache.taglibs.standard.tag.rt.core.OutTag) _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.get(org.apache.taglibs.standard.tag.rt.core.OutTag.class);
-    _jspx_th_c_005fout_005f1.setPageContext(_jspx_page_context);
-    _jspx_th_c_005fout_005f1.setParent(null);
-    // /WEB-INF/views/footer.jspx(23,25) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_c_005fout_005f1.setValue(new String(" | "));
-    int _jspx_eval_c_005fout_005f1 = _jspx_th_c_005fout_005f1.doStartTag();
-    if (_jspx_th_c_005fout_005f1.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
-      _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.reuse(_jspx_th_c_005fout_005f1);
-      return true;
-    }
-    _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.reuse(_jspx_th_c_005fout_005f1);
-    return false;
-  }
-
-  private boolean _jspx_meth_spring_005fmessage_005f2(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  spring:message
-    org.springframework.web.servlet.tags.MessageTag _jspx_th_spring_005fmessage_005f2 = (org.springframework.web.servlet.tags.MessageTag) _005fjspx_005ftagPool_005fspring_005fmessage_0026_005fcode_005fnobody.get(org.springframework.web.servlet.tags.MessageTag.class);
-    _jspx_th_spring_005fmessage_005f2.setPageContext(_jspx_page_context);
-    _jspx_th_spring_005fmessage_005f2.setParent(null);
-    // /WEB-INF/views/footer.jspx(24,45) name = code type = null reqTime = true required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_spring_005fmessage_005f2.setCode("global_language");
-    int[] _jspx_push_body_count_spring_005fmessage_005f2 = new int[] { 0 };
-    try {
-      int _jspx_eval_spring_005fmessage_005f2 = _jspx_th_spring_005fmessage_005f2.doStartTag();
-      if (_jspx_th_spring_005fmessage_005f2.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
-        return true;
-      }
-    } catch (Throwable _jspx_exception) {
-      while (_jspx_push_body_count_spring_005fmessage_005f2[0]-- > 0)
-        out = _jspx_page_context.popBody();
-      _jspx_th_spring_005fmessage_005f2.doCatch(_jspx_exception);
-    } finally {
-      _jspx_th_spring_005fmessage_005f2.doFinally();
-      _005fjspx_005ftagPool_005fspring_005fmessage_0026_005fcode_005fnobody.reuse(_jspx_th_spring_005fmessage_005f2);
-    }
-    return false;
-  }
-
-  private boolean _jspx_meth_c_005fout_005f2(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  c:out
-    org.apache.taglibs.standard.tag.rt.core.OutTag _jspx_th_c_005fout_005f2 = (org.apache.taglibs.standard.tag.rt.core.OutTag) _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.get(org.apache.taglibs.standard.tag.rt.core.OutTag.class);
-    _jspx_th_c_005fout_005f2.setPageContext(_jspx_page_context);
-    _jspx_th_c_005fout_005f2.setParent(null);
-    // /WEB-INF/views/footer.jspx(25,24) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
-    _jspx_th_c_005fout_005f2.setValue(new String(": "));
-    int _jspx_eval_c_005fout_005f2 = _jspx_th_c_005fout_005f2.doStartTag();
-    if (_jspx_th_c_005fout_005f2.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
-      _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.reuse(_jspx_th_c_005fout_005f2);
-      return true;
-    }
-    _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.reuse(_jspx_th_c_005fout_005f2);
-    return false;
-  }
-
-  private boolean _jspx_meth_util_005flanguage_005f0(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  util:language
-    org.apache.jsp.tag.web.util.language_tagx _jspx_th_util_005flanguage_005f0 = new org.apache.jsp.tag.web.util.language_tagx();
-    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_util_005flanguage_005f0);
-    _jspx_th_util_005flanguage_005f0.setJspContext(_jspx_page_context);
-    // /WEB-INF/views/footer.jspx(26,47) name = locale type = java.lang.String reqTime = true required = true fragment = false deferredValue = false expectedTypeName = java.lang.String deferredMethod = false methodSignature = null
-    _jspx_th_util_005flanguage_005f0.setLocale("en");
-    // /WEB-INF/views/footer.jspx(26,47) name = label type = java.lang.String reqTime = true required = true fragment = false deferredValue = false expectedTypeName = java.lang.String deferredMethod = false methodSignature = null
-    _jspx_th_util_005flanguage_005f0.setLabel("English");
-    _jspx_th_util_005flanguage_005f0.doTag();
-    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_util_005flanguage_005f0);
-    return false;
-  }
-
-  private boolean _jspx_meth_util_005flanguage_005f1(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  util:language
-    org.apache.jsp.tag.web.util.language_tagx _jspx_th_util_005flanguage_005f1 = new org.apache.jsp.tag.web.util.language_tagx();
-    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_util_005flanguage_005f1);
-    _jspx_th_util_005flanguage_005f1.setJspContext(_jspx_page_context);
-    // /WEB-INF/views/footer.jspx(27,53) name = locale type = java.lang.String reqTime = true required = true fragment = false deferredValue = false expectedTypeName = java.lang.String deferredMethod = false methodSignature = null
-    _jspx_th_util_005flanguage_005f1.setLocale("de");
-    // /WEB-INF/views/footer.jspx(27,53) name = label type = java.lang.String reqTime = true required = true fragment = false deferredValue = false expectedTypeName = java.lang.String deferredMethod = false methodSignature = null
-    _jspx_th_util_005flanguage_005f1.setLabel("Deubuilderh");
-    _jspx_th_util_005flanguage_005f1.doTag();
-    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_util_005flanguage_005f1);
-    return false;
-  }
-
-  private boolean _jspx_meth_util_005flanguage_005f2(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  util:language
-    org.apache.jsp.tag.web.util.language_tagx _jspx_th_util_005flanguage_005f2 = new org.apache.jsp.tag.web.util.language_tagx();
-    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_util_005flanguage_005f2);
-    _jspx_th_util_005flanguage_005f2.setJspContext(_jspx_page_context);
-    // /WEB-INF/views/footer.jspx(28,49) name = locale type = java.lang.String reqTime = true required = true fragment = false deferredValue = false expectedTypeName = java.lang.String deferredMethod = false methodSignature = null
-    _jspx_th_util_005flanguage_005f2.setLocale("es");
-    // /WEB-INF/views/footer.jspx(28,49) name = label type = java.lang.String reqTime = true required = true fragment = false deferredValue = false expectedTypeName = java.lang.String deferredMethod = false methodSignature = null
-    _jspx_th_util_005flanguage_005f2.setLabel("Espanol");
-    _jspx_th_util_005flanguage_005f2.doTag();
-    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_util_005flanguage_005f2);
-    return false;
-  }
-
-  private boolean _jspx_meth_util_005ftheme_005f0(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  util:theme
-    org.apache.jsp.tag.web.util.theme_tagx _jspx_th_util_005ftheme_005f0 = new org.apache.jsp.tag.web.util.theme_tagx();
-    org.apache.jasper.runtime.AnnotationHelper.postConstruct(_jsp_annotationprocessor, _jspx_th_util_005ftheme_005f0);
-    _jspx_th_util_005ftheme_005f0.setJspContext(_jspx_page_context);
-    _jspx_th_util_005ftheme_005f0.doTag();
-    org.apache.jasper.runtime.AnnotationHelper.preDestroy(_jsp_annotationprocessor, _jspx_th_util_005ftheme_005f0);
     return false;
   }
 
